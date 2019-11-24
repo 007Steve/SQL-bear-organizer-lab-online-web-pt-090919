e9369ed17b164a5c4d572328088e54b4d1a3db0c
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-11-24 06:22:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -11,6 +11,6 @@ INSERT INTO (id,name,age,gender,color,temperament,alive) VALUES (2,'Tabitha',21,
 
 INSERT INTO (id,name,age,gender,color,temperament,alive) VALUES (2,'Melissa',21,'M','BLACK','GOOD','alive');
 
-INSERT INTO (id,name,age,gender,color,temperament,alive) VALUES (2,'Grinch',21,'M','BLACK','GOOD','alive');
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES (2,'Grinch',21,'M','BLACK','GOOD','alive');
 
 INSERT INTO (id,name,age,gender,color,temperament,alive) VALUES (NULL,,NULL,NULL,NULL,NULL,NULL);
