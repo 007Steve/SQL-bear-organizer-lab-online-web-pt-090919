b26a28eb4a2a5993c052cb123fd94e4ee3cd300d
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-11-24 06:23:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO (id,name,age,gender,color,temperament,alive) VALUES (1,'Mr. Chocolate',20,'M','RED','GOOD','alive'); 
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES (1,'Mr. Chocolate',20,'M','RED','GOOD','alive'); 
 
-INSERT INTO (id,name,age,gender,color,temperament,alive) VALUES (2,'Rowdy',21,'M','BLACK','GOOD','alive');
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES (2,'Rowdy',21,'M','BLACK','GOOD','alive');
 
-INSERT INTO (id,name,age,gender,color,temperament,alive) VALUES (2,'Tabitha',21,'M','BLACK','GOOD','alive');
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES (2,'Tabitha',21,'M','BLACK','GOOD','alive');
 
-INSERT INTO (id,name,age,gender,color,temperament,alive) VALUES (2,'Sergeant Brown',21,'M','BLACK','GOOD','alive');
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES (2,'Sergeant Brown',21,'M','BLACK','GOOD','alive');
 
-INSERT INTO (id,name,age,gender,color,temperament,alive) VALUES (2,'Tabitha',21,'M','BLACK','GOOD','alive');
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES (2,'Tabitha',21,'M','BLACK','GOOD','alive');
 
 
-INSERT INTO (id,name,age,gender,color,temperament,alive) VALUES (2,'Melissa',21,'M','BLACK','GOOD','alive');
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES (2,'Melissa',21,'M','BLACK','GOOD','alive');
 
 INSERT INTO (name,age,gender,color,temperament,alive) VALUES (2,'Grinch',21,'M','BLACK','GOOD','alive');
 
-INSERT INTO (id,name,age,gender,color,temperament,alive) VALUES (NULL,,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES (NULL,,NULL,NULL,NULL,NULL,NULL);
